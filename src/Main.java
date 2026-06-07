42797b8a450982b948f53901052fa38eb40a1e39
Collin Zhong <czhong33@uwo.ca>
2026-06-06 20:48:17 -0400

    dec

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        System.out.println("Project: June 6th, 2026\nEstimated time of functional completion: August 1st, 2026");
     }
 }
