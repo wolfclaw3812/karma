1264a0a494c350b6181ec1e60b8f3a3171e0a6c4
wolfclaw3812 <collinzxy@gmail.com>
2026-08-16 10:44:45 -0400

    export more modules

--- src/module-info.java
+++ src/module-info.java
@@ -2,8 +2,12 @@ module karmic {
     requires transitive javafx.controls;
     requires transitive javafx.graphics;
     requires javafx.fxml;
+    requires com.fasterxml.jackson.databind;
+    requires com.fasterxml.jackson.annotation;
     opens GUI to javafx.graphics, javafx.fxml;
 
     exports GUI;
     exports Engine;
+    exports Combat;
+    exports EventListener;
 }
