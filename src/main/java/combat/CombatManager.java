f2328c485f361198b6e573e5943ce49498103b7c
wolfclaw3812 <collinzxy@gmail.com>
2026-08-17 23:13:18 -0400

    Renaming Damage to more apt Attack

--- src/main/java/combat/CombatManager.java
+++ src/main/java/combat/CombatManager.java
@@ -4,17 +4,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 import eventListener.CombatListeners;
-import eventListener.DamageListener;
+import eventListener.AttackListener;
 import eventListener.CombatListeners.PreDamageListener;
 
 public class CombatManager {
-    private final List<DamageListener> damageListeners = new ArrayList<>();
+    private final List<AttackListener> damageListeners = new ArrayList<>();
     private final List<PreDamageListener> preDamageEventListeners = new ArrayList<>();
     public CombatManager(){
 
     }
 
-    public void addDamageEventListener(DamageListener listener){
+    public void addDamageEventListener(AttackListener listener){
         if (listener == null) {
             throw new IllegalArgumentException("Listener cannot be null");
         }else{
@@ -22,12 +22,12 @@ public class CombatManager {
         }
     }
 
-    public void removeDamageEventListener(CombatListeners.DamageListener listener){
+    public void removeDamageEventListener(AttackListener.DamageListener listener){
         damageListeners.remove(listener);
     }
 
     public void dealDamage(Combatant source, Combatant target, Damage damage){
-        DamageEvent event = new DamageEvent(source, target, damage);
+        AttackEvent event = new AttackEvent(source, target, damage);
         
         // Notify listeners
         // pre-damage events
@@ -35,7 +35,7 @@ public class CombatManager {
             listener.onPreDamage(event);
         }
         target.takeDamage(damage);
-        for (DamageListener listener : damageListeners) {
+        for (AttackListener listener : damageListeners) {
             listener.onDamage(event);
         }
         
